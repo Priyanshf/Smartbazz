90d24319c78ce93e5efef40c4f1a5655a86bdd54
Priyanshf <143727446+Priyanshf@users.noreply.github.com>
2024-10-23 16:05:44 +0530

    Add files via upload

---
+++ DriverManager.java
@@ -0,0 +1,23 @@
+
+import com.sun.jdi.connect.spi.Connection;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+class DriverManager {
+
+    static Connection getConnection(String string, String root, String yourpassword) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void getConnection(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
